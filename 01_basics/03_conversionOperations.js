2594566486b89316f9e10a8aca4545906846d0a5
Arsh Sharma <97118125+Arsh-sharma@users.noreply.github.com>
2023-12-19 16:52:36 +0000

    comparison

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = "33";
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valInNum = Number(score)
 
-console.log(typeof valInNum)
+// console.log(typeof valInNum)
 
 // if orignally the score was not a number like "33abc" then also this conversion would work but the value of valInNum will be Nan
 //Not A Number
@@ -15,19 +15,43 @@ console.log(typeof valInNum)
 //string => Nan
 //typeof Nan is a number
 
-let isLoggedIn = 1
-let test = Boolean(isLoggedIn)
-console.log(test)
+// let isLoggedIn = 1
+// let test = Boolean(isLoggedIn)
+// console.log(test)
 
-let isLoggedOut = 0
-let testOut = Boolean(isLoggedOut)
-console.log(testOut)
+// let isLoggedOut = 0
+// let testOut = Boolean(isLoggedOut)
+// console.log(testOut)
 
-let name = ""
-let test2 = Boolean(name)
-console.log(test2)
+// let name = ""
+// let test2 = Boolean(name)
+// console.log(test2)
 
-let name2 = "arshiee"
-let test3 = Boolean(name2)
-console.log(test3)
+// let name2 = "arshiee"
+// let test3 = Boolean(name2)
+// console.log(test3)
 
+/* ************************************OPERATIONS****************************************************** */
+let val = 3
+let negVal = -val
+// console.log(negVal)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**4)
+// console.log(2/2)
+// console.log(2%2)
+
+// let greeting = "Hi "
+// let name = "Arsh"
+// console.log(greeting+name)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+1+"2")
+console.log(!+1+9)
+
+//prefix postfix ++
+//rest in notes
