f1cf4d2754f4dc8d07ca202f2b8c36024f38f385
Arsh Sharma <97118125+Arsh-sharma@users.noreply.github.com>
2023-12-19 13:42:05 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as new version
+
+// alert(3+3) //does not runs in node but runs in browser
+
+console.log("hello"); 
+console.log("yayyyyyyyyyyyy");  //use proper syntax
+
+let name = "arsh";
+let age = 18;
+let loggedIn = true;
+
+//number
+//bigint
+//boolean => true/false
+//string => ""
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const id = 1233
+let name = "arsh"
+email = "udk@hgsxyk.com"
+var pass = "fsxfsiybj"
+
+console.table([id,name,email,pass])
+
+// id = 6654  // can not be changed
+
+name = "arshiee"
+email = "syfctsycfy@hjg.com"
+pass = "jcdhyugdy"
+let surname;
+
+console.table([id,name,email,pass,surname])
+
+/* DONT USE var due to issue in block and functional scope */
